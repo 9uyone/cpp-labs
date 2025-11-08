46800738f36d48dd7974056d431e968df32db65d
Ostap Haivanovych <97313368+9uyone@users.noreply.github.com>
2025-11-08 21:02:54 +0200

    Draft version of Lab 6

--- labs_6_10/lib/screens/home_page.dart
+++ labs_6_10/lib/screens/home_page.dart
@@ -10,15 +10,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final labs = [
-      {'title': 'Лаба 6: ', 'page': const Lab6Page()},
-      {'title': 'Лаба 7: ', 'page': const Lab7Page()},
-      {'title': 'Лаба 8: ', 'page': const Lab8Page()},
-      {'title': 'Лаба 9: ', 'page': const Lab9Page()},
+      {'title': 'Лаба 6: Text, Компонування класів', 'page': const Lab6Page()},
+      {
+        'title': 'Лаба 7: RichText, Stack, Positioned',
+        'page': const Lab7Page(),
+      },
+      {
+        'title': 'Лаба 8: StatelessWidget, MaterialApp',
+        'page': const Lab8Page(),
+      },
+      {'title': 'Лаба 9: StatefulWidget', 'page': const Lab9Page()},
     ];
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Лаби')),
-      body: ListView.builder(
+      appBar: AppBar(title: const Text('Лабораторна 10')),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => const Divider(),
         itemCount: labs.length,
         itemBuilder: (context, index) {
           final lab = labs[index];
--- labs_6_10/lib/screens/lab6_page.dart
+++ labs_6_10/lib/screens/lab6_page.dart
@@ -7,8 +7,51 @@ class Lab6Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Лаба 6')),
-      body: const Center(
-        child: Text('Контент лаби 6', style: TextStyle(fontSize: 20)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 200, 20, 20),
+            child: Row(
+              spacing: 20,
+              children: <Widget>[
+                ConstrainedBox(
+                  constraints: const BoxConstraints.expand(
+                    width: 100,
+                    height: 120,
+                  ),
+                  child: Container(
+                    color: Colors.amber,
+                    child: const Text('''Ще не вмерла України і слава, і воля,
+Ще нам, браття молодії, усміхнеться доля.''', style: TextStyle(fontSize: 12)),
+                  ),
+                ),
+                //const SizedBox(width: 20),
+                Expanded(
+                  child: Container(
+                    color: Colors.lightBlue,
+                    child: const Text(
+                      '''Згинуть наші воріженьки, як роса на сонці.
+Запануєм і ми, браття, у своїй сторонці
+''',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(400, 150, 0, 0),
+            child: Container(
+              height: 100,
+              color: Colors.green.shade100,
+              child: Text('''Душу, тіло ми положим за нашу свободу.
+І покажем, що ми, браття, козацького роду!''', style: TextStyle(fontSize: 12)),
+            ),
+          ),
+        ],
       ),
     );
   }
